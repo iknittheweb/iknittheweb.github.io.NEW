18c5bf8a51da3f9205a712c6fd5a859080ef4309
idlehands1969 <109295206+idlehands1969@users.noreply.github.com>
2025-10-22 03:54:56 -0500

    chore(config): add Stylelint config, update Cypress and package scripts

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: 'xrecd4',
   e2e: {
     setupNodeEvents(on, config) {
       // Percy does not require plugin setup here
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,1 +1,8 @@
 import '@percy/cypress';
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // Ignore Sentry module errors during tests
+  if (err.message && err.message.includes('@sentry/browser')) {
+    return false;
+  }
+});
