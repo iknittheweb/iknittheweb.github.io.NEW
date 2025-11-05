f266d82fdcff552746763b9283dc3730af47557f
idlehands1969 <109295206+idlehands1969@users.noreply.github.com>
2025-11-05 05:57:26 -0600

    test(navigation): add visibility checks for accessibility test
    Added robust existence and visibility checks before accessibility attribute assertions to ensure navigation element is present and visible before testing.
    
    No breaking changes.

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -5,10 +5,11 @@ describe('Navigation UI/Interaction', () => {
     cy.get('#menuTopNav').should('exist');
   });
   it('should set accessibility attributes on navigation', () => {
-    cy.get('#menuTopNav').should('exist');
-    cy.get('#menuTopNav').should('exist').and('have.attr', 'aria-hidden', 'true');
+    cy.get('#menuTopNav').should('exist').and('be.visible');
+    cy.get('#menuTopNav').should('exist').and('be.visible').and('have.attr', 'aria-hidden', 'true');
     cy.get('#menuTopNav')
       .should('exist')
+      .and('be.visible')
       .then(($el) => {
         if ($el.attr('inert') !== undefined) {
           expect($el).to.have.attr('inert');
