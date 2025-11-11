fe335adebb7fb49c4bd38351d6f6b74a4c466e24
idlehands1969 <109295206+idlehands1969@users.noreply.github.com>
2025-11-11 09:23:02 -0600

    Update environment and build scripts
    Update .env and supporting build scripts for new CSP and FOUC handling. Ensures environment and build process are in sync with new deployment requirements.

--- remove-link-onload.cjs
+++ remove-link-onload.cjs
@@ -9,12 +9,15 @@ const htmlDir = process.cwd();
 
 function cleanLinkTags(filePath) {
   let html = fs.readFileSync(filePath, 'utf8');
-  // Remove onload="this.media='all'" from <link rel="stylesheet">
-  html = html.replace(/(<link[^>]+rel=["']stylesheet["'][^>]+)onload=["']this\.media='all'["']/gi, '$1');
-  // Remove media="print" from <link rel="stylesheet">
-  html = html.replace(/(<link[^>]+rel=["']stylesheet["'][^>]+)media=["']print["']/gi, '$1');
-  // Remove any leftover double spaces
+  // Remove onload="this.media='all'" (single or double quotes, any whitespace)
+  html = html.replace(/\s*onload\s*=\s*(["'])this\.media='all'\1/gi, '');
+  // Remove media="print" (single or double quotes, any whitespace)
+  html = html.replace(/\s*media\s*=\s*(["'])print\1/gi, '');
+  // Remove extra whitespace between attributes
+  html = html.replace(/<link\s+/gi, '<link ');
   html = html.replace(/\s{2,}/g, ' ');
+  // Remove trailing whitespace before closing >
+  html = html.replace(/\s*>/g, '>');
   fs.writeFileSync(filePath, html);
   console.log(`Cleaned: ${filePath}`);
 }
